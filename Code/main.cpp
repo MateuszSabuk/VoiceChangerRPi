2c7fdf3239ca0ad5896429d86800314f27b3ac99
MateuszSabuk <mateusz@sabuk.pl>
2024-11-25 01:15:08 +0100

    Keyboard control

--- Code/main.cpp
+++ Code/main.cpp
@@ -8,9 +8,13 @@
 #include <cstring>
 #include <mutex>
 #include <vector>
+#include <thread> // For capturing key presses
+#include <ncurses.h> // For capturing key presses
 
 #define BUFFER_SIZE 8192
 
+float volume_value = 1;
+float pitch_value = 1;
 
 jack_port_t *input_port;
 jack_port_t *output_port;
@@ -24,7 +28,7 @@ static void signal_handler ( int sig )
 }
 
 void jack_shutdown ( void *arg )
-{
+{// For capturing key presses
     free ( input_port );
     free ( output_port );
     exit ( 1 );
@@ -33,7 +37,7 @@ void jack_shutdown ( void *arg )
 std::vector<jack_default_audio_sample_t> ring_buffer(BUFFER_SIZE);
 std::mutex ring_mutex; // Mutex to ensure thread-safe access to the ring buffer
 size_t write_index = 0;
-size_t read_index = 0;
+float read_index = 0;
 
 int process(jack_nframes_t nframes, void *arg)
 {
@@ -47,17 +51,48 @@ int process(jack_nframes_t nframes, void *arg)
 
     // Write data into the ring buffer
     for (unsigned int i = 0; i < nframes; ++i) {
-        ring_buffer[write_index] = in[i];
+        ring_buffer[write_index] = in[i] * volume_value;
         write_index = (write_index + 1) % BUFFER_SIZE;
     }
 
     // Read data from the ring buffer and write it to the output
     for (unsigned int i = 0; i < nframes; i++) {
-        out[i] = ring_buffer[read_index];
-        read_index = (read_index + bool(i%7)) % BUFFER_SIZE;
+        out[i] = ring_buffer[int(read_index)];
+        read_index += pitch_value;
+        if (read_index >= BUFFER_SIZE) read_index -= BUFFER_SIZE;
     }
 
     return 0;
+}
+
+// Thread function to handle key presses
+void key_press_listener()
+{
+    initscr();             // Initialize ncurses
+    cbreak();              // Disable line buffering
+    noecho();              // Disable echoing of characters
+    keypad(stdscr, TRUE);  // Enable function and arrow keys
+    timeout(100);          // Non-blocking input
+
+    while (true) {
+        int ch = getch();
+        switch (ch) {
+        case KEY_UP:
+            volume_value += 0.05;
+            break;
+        case KEY_DOWN:
+            volume_value -= 0.05;
+            break;
+        case KEY_LEFT:
+            pitch_value -= 0.01;
+            break;
+        case KEY_RIGHT:
+            pitch_value += 0.01;
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 int main ( int argc, char *argv[] )
@@ -82,6 +117,9 @@ int main ( int argc, char *argv[] )
     jack_set_process_callback ( client, process, 0 );
 
     jack_on_shutdown ( client, jack_shutdown, 0 );
+
+    // Start the key press listener in a separate thread
+    std::thread key_listener_thread(key_press_listener);
 
     // Register an input port
     input_port = jack_port_register(
